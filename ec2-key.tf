13ed484456c6d67dccce049892b31cbd2e968c21
saikrishna <saikrishna2733@gmail.com>
2021-08-25 16:42:40 +0530

    1. Error Message while Destroying the Resources

--- ec2-key.tf
+++
@@ -1,5 +0,0 @@
-############################AWS KEY PAIR DEPLOYMENT #################3
-resource "aws_key_pair" "deployer" {
-  key_name   = var.keypairname
-  public_key = var.ec2-key-public-key
-}
